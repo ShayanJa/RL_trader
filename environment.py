701fd3d42d3da1879cb65b6bab246bd65dd6bd65
shayanja <shay.talebi@gmail.com>
2020-04-27 18:05:59 -0500

    Add MACD to reward func

--- environment.py
+++ environment.py
@@ -1,6 +1,7 @@
 import abc
 import tensorflow as tf
 import numpy as np
+import ta
 
 from tf_agents.environments import py_environment
 from tf_agents.environments import tf_environment
@@ -14,20 +15,30 @@ from tf_agents.trajectories import time_step as ts
 tf.compat.v1.enable_v2_behavior()
 
 class BitcoinEnvironment(py_environment.PyEnvironment):
-  def __init__(self, initial_balance, price_data, price_history_t):
+  def __init__(self, initial_balance, price_data, price_history_t, mean_history_t, macd_t, fast_ema, slow_ema):
     self.t = 0
+    self.fast_ema = fast_ema
+    self.slow_ema = slow_ema
     self.price_data = price_data
     self.price_history_t = price_history_t
+    self.mean_history_t = mean_history_t
+    self.macd_t = macd_t
     self.initial_balance = initial_balance
     self.balance = initial_balance
     self.cash_balance = initial_balance
-    self.position_increment = .3
+    self.position_increment = .1
+    self.max_drawdown = initial_balance
     self.positions = []
     self._action_spec = array_spec.BoundedArraySpec(
       shape=(), dtype=np.int32, minimum=0, maximum=2, name='action')
     self._observation_spec = array_spec.BoundedArraySpec(
-      shape=(price_history_t+1,), dtype=np.float32, name="observation")
+      shape=(price_history_t+macd_t+1,), dtype=np.float32, name="observation")
     self.return_history = [self.price_data.iloc[self.t-k, :]['Close'] - self.price_data.iloc[self.t-k-1, :]['Close']  for k in reversed(range(self.price_history_t))]
+    self.mean_data = self.price_data.rolling(20, min_periods=1).mean()
+    # self.mean_history = [self.mean_data.iloc[self.t-k, :]['Close'] for k in reversed(range(self.mean_history_t))]
+    self.MACD_trend = ta.trend.ema_indicator(self.price_data['Close'], self.fast_ema) - ta.trend.ema_indicator(self.price_data['Close'], self.slow_ema)
+    self.MACD_trend = self.MACD_trend.dropna().tolist()
+    self.MACD = [self.MACD_trend[self.t-k] for k in reversed(range(self.macd_t))]
     self._episode_ended = False
 
   def observation_spec(self):
@@ -40,8 +51,13 @@ class BitcoinEnvironment(py_environment.PyEnvironment):
     # action = 0: Hold, 1: Buy, 2: Sell
     rewards = 0
     if action == 0:
-      dp = self.price_data.iloc[self.t, :]['Close'] - self.price_data.iloc[self.t-1, :]['Close']
-      rewards = dp 
+      pass
+      # dp = self.price_data.iloc[self.t, :]['Close'] - self.price_data.iloc[self.t-1, :]['Close']
+      # for p in self.positions:
+      #   rewards += 0.1 * dp
+      #   rewards += .2 * (self.MACD[-1])
+        # if dp < 0:
+        #   rewards *= 2
     elif action == 1:
       p = self.price_data.iloc[self.t, :]['Close'] * self.position_increment
       if p > self.cash_balance:
@@ -50,6 +66,8 @@ class BitcoinEnvironment(py_environment.PyEnvironment):
       else:
         self.cash_balance -= p 
         self.positions.append(self.price_data.iloc[self.t, :]['Close'])
+        # rewards = .5 * self.mean_data.iloc[self.t, :]['Close'] - self.price_data.iloc[self.t, :]['Close'] 
+        rewards += .5 * (self.MACD[-1])
     elif action == 2:
       rewards, profits = 0, 0
       for p in self.positions:
@@ -62,14 +80,17 @@ class BitcoinEnvironment(py_environment.PyEnvironment):
     self.balance = self.cash_balance
     for p in self.positions:
       self.balance += self.price_data.iloc[self.t, :]['Close'] * self.position_increment
+    
+    self.max_drawdown = min(self.max_drawdown, self.balance)
     self.t += 1
     self.return_history.pop(0)
     self.return_history.append(self.price_data.iloc[self.t, :]['Close'] - self.price_data.iloc[self.t-1, :]['Close'])
-
+    self.MACD.pop(0)
+    self.MACD.append(self.MACD_trend[self.t])
     if self.t == len(self.price_data -1):
       self._episode_ended = True
 
-    self._state = [self.balance] + self.return_history
+    self._state = [self.balance] + self.return_history + self.MACD
     
     return ts.transition(
       np.array(self._state, dtype=np.float32), reward=rewards, discount=0.8)
@@ -85,5 +106,14 @@ class BitcoinEnvironment(py_environment.PyEnvironment):
     self.cash_balance = self.initial_balance
     self.positions = []
     self.return_history = [self.price_data.iloc[self.t-k, :]['Close'] - self.price_data.iloc[self.t-k-1, :]['Close']  for k in reversed(range(self.price_history_t))]
-    self._state = [self.balance] + self.return_history
+    self.mean_history = [self.mean_data.iloc[self.t-k, :]['Close'] for k in reversed(range(self.mean_history_t))]
+    self.MACD = [self.MACD_trend[self.t-k] for k in reversed(range(self.macd_t))]
+    self._state = [self.balance] + self.return_history + self.MACD
     return ts.restart(np.array(self._state, dtype=np.float32))
+
+  def buy_and_hold(self):
+    amount =  self.initial_balance / self.price_data.iloc[0, :]['Close']
+    return self.price_data * amount
+  
+  def get_max_drawdown(self):
+    return (self.max_drawdown-self.initial_balance)/self.initial_balance
--- trader.py
+++ trader.py
@@ -18,40 +18,26 @@ from tf_agents.drivers import dynamic_episode_driver
 
 from environment import BitcoinEnvironment
 import datetime as dt
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def compute_avg_return(environment, policy, num_episodes=10):
+def compute_performance(environment, policy):
 
+    time_step = environment.reset()
     total_return = 0.0
-    for _ in range(num_episodes):
+    balance = [time_step.observation[0][0]]
 
-        time_step = environment.reset()
-        episode_return = 0.0
+    while not time_step.is_last():
+      action_step = policy.action(time_step)
+      time_step = environment.step(action_step.action)
+      total_return += time_step.reward
+      balance.append(time_step.observation[0][0])
+      print(time_step.observation)
 
-        while not time_step.is_last():
-            action_step = policy.action(time_step)
-            time_step = environment.step(action_step.action)
-            episode_return += time_step.reward
-            total_return += episode_return
+    return total_return[0], balance
 
-    avg_return = total_return / num_episodes
-    return avg_return.numpy()[0]
-  
-def compute_balance(environment, policy):
-  
-  time_step = environment.reset()
-  balance = [time_step.observation[0][0]]
-
-  while not time_step.is_last():
-    action_step = policy.action(time_step)
-    time_step = environment.step(action_step.action)
-    balance.append(time_step.observation[0][0])
-
-  return balance
-
-
-num_iterations = 50000  # @param
+num_iterations = 10000  # @param
 
 replay_buffer_capacity = 100000  # @param
 
@@ -64,10 +50,6 @@ log_interval = 200  # @param
 num_eval_episodes = 2  # @param
 eval_interval = 1000  # @param
 
-initial_balance = 10000  # @param
-training_duration = 3000  # @param
-eval_duration = 1000  # @param
-
 date_split = dt.datetime(2018, 3, 16, 1, 0)  # @param
 
 # Split data into training and test set
@@ -75,29 +57,36 @@ prices = pd.read_csv('btcusd.csv', parse_dates=True, index_col=0)
 train = prices[:date_split]
 test = prices[date_split:]
 
+initial_balance = 10000  # @param
+training_duration = 200  # @param
+eval_duration = 200 # @param
+
+
 # Create Environments
-train_py_env = wrappers.TimeLimit(BitcoinEnvironment(initial_balance, train, 15), duration=training_duration)
-eval_py_env = wrappers.TimeLimit(BitcoinEnvironment(initial_balance, test, 15), duration=eval_duration)
+train_py_env = wrappers.TimeLimit(BitcoinEnvironment(initial_balance, train, 15, 15, 10, 12, 26), duration=training_duration)
+eval_py_env = wrappers.TimeLimit(BitcoinEnvironment(initial_balance, test, 15, 15, 10, 12, 26 ), duration=eval_duration)
+test_py_env = wrappers.TimeLimit(BitcoinEnvironment(initial_balance, test, 15, 15, 10, 12, 26 ), duration=len(test)-1)
 
 train_env = tf_py_environment.TFPyEnvironment(train_py_env)
 eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
+test_env = tf_py_environment.TFPyEnvironment(test_py_env)
 
 # Initialize Q Network
 q_net = q_network.QNetwork(
-        train_env.observation_spec(),
-        train_env.action_spec(),
-        fc_layer_params=fc_layer_params)
+  train_env.observation_spec(),
+  train_env.action_spec(),
+  fc_layer_params=fc_layer_params)
 
 optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate)
 
 train_step_counter = tf.compat.v2.Variable(0)
 
 tf_agent = dqn_agent.DqnAgent(
-        train_env.time_step_spec(),
-        train_env.action_spec(),
-        q_network=q_net,
-        optimizer=optimizer,
-        train_step_counter=train_step_counter)
+  train_env.time_step_spec(),
+  train_env.action_spec(),
+  q_network=q_net,
+  optimizer=optimizer,
+  train_step_counter=train_step_counter)
 
 tf_agent.initialize()
 
@@ -105,9 +94,9 @@ eval_policy = tf_agent.policy
 collect_policy = tf_agent.collect_policy
 
 replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
-        data_spec=tf_agent.collect_data_spec,
-        batch_size=train_env.batch_size,
-        max_length=replay_buffer_capacity)
+  data_spec=tf_agent.collect_data_spec,
+  batch_size=train_env.batch_size,
+  max_length=replay_buffer_capacity)
 
 replay_observer = [replay_buffer.add_batch]
 
@@ -165,19 +154,26 @@ for i in range(num_iterations):
   step = tf_agent.train_step_counter.numpy()
 
   if step % log_interval == 0:
-      print('step = {0}: loss = {1}'.format(step, train_loss.loss))
-      episode_len.append(train_metrics[3].result().numpy())
-      print('Average episode length: {}'.format(train_metrics[3].result().numpy()))
+    print('step = {0}: loss = {1}'.format(step, train_loss.loss))
+    episode_len.append(train_metrics[3].result().numpy())
+    print('Average episode length: {}'.format(train_metrics[3].result().numpy()))
 
   if step % eval_interval == 0:
-      avg_reward = compute_avg_return(eval_env, tf_agent.policy, num_eval_episodes)
-      portfolio_balance = compute_balance(eval_env, tf_agent.policy)
-      print('step = {0}: Average Reward = {1}: Ending Portfolio Balance = {2}'.format(step, avg_reward, portfolio_balance[-1]))
-  
+    reward, portfolio_balance = compute_performance(eval_env, tf_agent.policy)
+    print('step = {0}: Average Reward = {1}: Ending Portfolio Balance = {2}'.format(step, reward, portfolio_balance[-1]))
+
 # my_policy = tf_agent.collect_policy
 # saver = policy_saver.PolicySaver(my_policy, batch_size=None)
 # saver.save('policy_trader')
 
+# Compare against Buy and hold
+reward, portfolio_balance = compute_performance(test_env, tf_agent.policy)
+bnh = eval_py_env.buy_and_hold().head(eval_duration+1)
+portfolio_balance = pd.DataFrame(data={'Close': np.array(portfolio_balance)}, index=bnh.index)
+print("RL GAIN = {}: BUY/HOLD = {}".format(portfolio_balance.iloc[-1,:]['Close']-portfolio_balance.iloc[0,:]['Close'], bnh.iloc[-1]['Close']-bnh.iloc[0]['Close']))
+bnh['Close'].plot()
+portfolio_balance['Close'].plot()
 
-plt.plot(portfolio_balance, 'b')
+history = pd.DataFrame(data={"Buy/Hold": bnh['Close'], "RL": portfolio_balance['Close']}, index=bnh.index)
+history.to_csv("results/trader-{}".format(dt.datetime.now()))
 plt.show()
